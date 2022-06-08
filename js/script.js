91bba1ada147e4f6f66fe9230746500facc7ea2c
Majkic Milos <mlmajkicmlmajkic@gmail.com>
2022-06-08 16:43:03 +0200

    added some examples

--- js/script.js
+++ js/script.js
@@ -128,3 +128,123 @@ function arrayDiff(a, b) {{
 }
 
 }
+
+// vezba bankaccounts
+
+class User {
+  constructor(name, balance) {
+      this.name = name
+      this.balance = balance
+  }
+  credit() {
+      this.balance += 1000
+  }
+  describe() {
+      console.log(`${this.name} has ${this.balance}$ in his account`)
+  }
+}
+
+
+const seul = new User('Seul', 0)
+const paul = new User('Paul', 0)
+
+//vezba RPG Character
+
+class Character {
+  constructor(name, health, streangth, inventory) {
+      this.name = name
+      this.health = health
+      this.streangth = streangth
+      this.experience = 0
+      this.inventory = inventory
+  }
+  attack(target) {
+      // if attack is impossible
+
+      if (target.health <= 0) {
+          console.log(`The target is alredy dead!`)
+
+      } else if (this.health <= 0) {
+          console.log(`This character has been slain,no attack has been done.`)
+
+
+      } else if (this.health > 0) { //attack is possible
+
+          let dmg = this.streangth
+          target.health -= dmg
+
+          if (target.health <= 0) {
+              console.log(`${this.name} has slain ${target.name}, and gained 10 XP, ${target.inventory[0]} gold and ${target.inventory[1]} key(s).`)
+              this.experience += 10
+              for (let i = 0; i < target.inventory.length; i++) {
+                  this.inventory[i] += target.inventory[i]
+              }
+          } else {
+              console.log(`${this.name} has inflicted ${dmg} damage to ${target.name}.${target.name} has ${target.health} health points left.`)
+          }
+      }
+
+
+
+
+  }
+
+  describe() {
+      return `${this.name} has ${this.health} health points,${this.experience} experience,${this.streangth} strength points. In invetory ${this.name} has ${this.inventory[0]} gold and ${this.inventory[1]} key(s)`
+  }
+
+}
+
+const aurora = new Character("Aurora", 100, 50, [50, 1])
+const travis = new Character("Travis", 100, 50, [50, 1])
+
+
+const governmentForms = [
+  {
+      name: "Plutocracy",
+      definition: "Rule by the wealthy"
+  },
+  {
+      name: "Oligarchy",
+      definition: "Rule by a small number of people"
+  },
+  {
+      name: "Kleptocracy",
+      definition: "Rule by the thieves"
+  },
+  {
+      name: "Theocracy",
+      definition: "Rule by a religious elite"
+  },
+  {
+      name: "Democracy",
+      definition: "Rule by the people"
+  },
+  {
+      name: "Autocracy",
+      definition: "Rule by a single person"
+  }
+];
+
+const arrNames = arr => arr.map(arr => arr.name)
+const filterEndingCy = arr => arr.filter(element => element.substr(-2) == 'cy')
+
+const arrandFilter = (arr, filterarr) => filterarr(arr)
+
+console.log(arrandFilter(arrNames(governmentForms), filterEndingCy))
+
+//console.log(arrandfilter(arrNames(governmentForms),filterEndingCy))
+// TODO: compute the formsEndingWithCy array
+
+// Should show ["Plutocracy", "Kleptocracy", "Theocracy", "Democracy", "Autocracy"]
+//console.log(formsEndingWithCy);
+
+
+const arrays = [[1, 4], [11], [3, 5, 7]];
+
+const suminternal = arr => arr.map(x => x.reduce((acc, e) => acc + e, 0))
+const arraysSum = suminternal(arrays).reduce((acc, e) => acc + e, 0)
+
+// TODO: compute the value of the arraysSum variable
+
+console.log(arraysSum); // Should show 31
