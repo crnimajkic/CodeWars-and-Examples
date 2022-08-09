ff89bdd7774ef67f11726938929efb284bd42220
Miloš Majkić <105221872+crnimajkic@users.noreply.github.com>
2022-08-09 16:58:03 +0200

    another solution

--- js/script.js
+++ js/script.js
@@ -367,3 +367,12 @@ function hello(name) {
   }
   return `Hello, ${name}!`
 }
+
+//small problem
+function problem(x){
+  
+  if(typeof x === 'string'){
+    return "Error"
+  }
+  return x*50 + 6
+}
